52bbc91bb63dbeb48b0b9bb8bb00a36d5e4593c8
Sameer HBTU <srivastavasameer2002@gmail.com>
2024-01-14 12:01:29 +0530

    updated index in components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./header/Header";
+import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from './Logo'
