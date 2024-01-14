d87558af6a94d55082a6938e3bf3e86ea3138bc9
Sameer HBTU <srivastavasameer2002@gmail.com>
2024-01-14 15:56:23 +0530

    added md class in Home and All Post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React,{useEffect, useState} from 'react'
 import appwriteService from '../appwrite/config'
 import { Container, PostCard } from '../components'
 import { useNavigate } from 'react-router-dom';
-import { login,  } from '../store/authSlice';
 function Home() {
     const navigate= useNavigate();
     const [posts, setPosts]= useState([]);
@@ -34,7 +33,7 @@ function Home() {
              <Container>
             <div className='flex flex-wrap'>
                 {posts.map((post)=>(
-                    <div key={post.$id} className='p-2 w-1/4'>
+                    <div key={post.$id} className='p-2 md:w-1/4'>
                         <PostCard {...post}/>
                     </div>
                 ))}
